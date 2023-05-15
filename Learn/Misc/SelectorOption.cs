29d94a5ad8b249f25fe6f3882b4e6cb33efcc727
Marat Ashrafzianov <marat.ashraf@yandex.ru>
2023-05-16 01:06:36 +0300

    SelectorOption class was added

---
+++ Learn/Misc/SelectorOption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.Misc
+{
+    public class SelectorOption
+    {
+        int _Id;
+        string _Title;
+        
+        public string Title
+        {
+            get
+            {
+                return _Title;
+            }
+        }
+        public SelectorOption(int Id, string Title)
+        {
+            _Id = Id;
+            _Title = Title;
+        }
+    }
+}
